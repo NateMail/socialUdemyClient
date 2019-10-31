89af5e596a8a5fa1b9458b261f148686c6d93579
nathanmailhiot <nathanjm12@outlook.com>
2019-10-31 08:09:04 -0400

    Placeholder SignUp Component

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Signup from './components/user/SignUp';
 
 const MainRouter = () => (
   <div>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
     </Switch>
   </div>
 );
---
+++ src/components/user/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sign up</h2>
+        <p>Here</p>
+      </div>
+    );
+  }
+}
+
+export default Signup;
