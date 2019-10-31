d543157bba017733e39169071ca2c6e8a716e76e
nathanmailhiot <nathanjm12@outlook.com>
2019-10-31 08:15:55 -0400

    Set up for signup form

--- src/components/user/SignUp.js
+++ src/components/user/SignUp.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      error: ''
+    };
+  }
   render() {
     return (
-      <div>
-        <h2>Sign up</h2>
-        <p>Here</p>
+      <div className="container">
+        <h2 className="mt-5 mb-5">Sign up</h2>
+
+        <form>
+          <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input type="password" className="form-control" />
+          </div>
+          <button className="btn btn-raised btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
