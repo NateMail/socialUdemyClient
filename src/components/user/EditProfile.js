45d03ae3431dffaa7c0dbe7a0d8a04e82a6df46b
nathanmailhiot <nathanjm12@outlook.com>
2019-11-01 12:02:57 -0400

    Able to get user profile picture in edit profile

--- src/components/user/EditProfile.js
+++ src/components/user/EditProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { isAuthenticated } from '../../auth';
 import { read, update } from './apiUser';
 import { Redirect } from 'react-router-dom';
+import DefaultProfile from '../../images/avatar.jpg';
 
 class EditProfile extends Component {
   constructor() {
@@ -147,6 +148,10 @@ class EditProfile extends Component {
       return <Redirect to={`/user/${id}`} />;
     }
 
+    const photoUrl = id
+      ? `${process.env.REACT_APP_API_URL}/user/photo/${id}`
+      : DefaultProfile;
+
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Edit Profile</h2>
@@ -163,6 +168,7 @@ class EditProfile extends Component {
         ) : (
           ''
         )}
+        <img src={photoUrl} alt={name} />
         {this.editProfileForm(name, email, password)}
       </div>
     );
