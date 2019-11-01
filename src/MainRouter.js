39a82157212443cdddc9cd6fbccbfb1d87a01558
nathanmailhiot <nathanjm12@outlook.com>
2019-11-01 10:31:47 -0400

    User can edit profile

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -6,6 +6,7 @@ import Signup from './components/user/SignUp';
 import Signin from './components/user/Signin';
 import Profile from './components/user/Profile';
 import Users from './components/user/Users';
+import EditProfile from './components/user/EditProfile';
 
 const MainRouter = () => (
   <div>
@@ -15,6 +16,7 @@ const MainRouter = () => (
       <Route exact path="/users" component={Users} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/signin" component={Signin} />
+      <Route exact path="/user/edit/:userId" component={EditProfile} />
       <Route exact path="/user/:userId" component={Profile} />
     </Switch>
   </div>
