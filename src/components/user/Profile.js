488c6e0f1ce7cd8de0dd47abead6695715397a62
nathanmailhiot <nathanjm12@outlook.com>
2019-11-01 15:55:32 -0400

    Able to show followers and following

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -5,6 +5,7 @@ import { read } from './apiUser';
 import DefaultProfile from '../../images/avatar.jpg';
 import DeleteUser from './DeleteUser';
 import FollowProfileButton from './FollowProfileButton';
+import ProfileTabs from './ProfileTabs';
 
 class Profile extends Component {
   constructor() {
@@ -111,6 +112,10 @@ class Profile extends Component {
             <hr />
             <p className="lead">{user.about}</p>
             <hr />
+            <ProfileTabs
+              followers={user.followers}
+              following={user.following}
+            />
           </div>
         </div>
       </div>
