733b0f53e1b67fb7084e9eaadbf4179c7cd9fb62
nathanmailhiot <nathanjm12@outlook.com>
2019-11-04 15:11:58 -0500

    Get users list of posts

--- src/components/post/apiPost.js
+++ src/components/post/apiPost.js
@@ -12,3 +12,13 @@ export const create = (userId, token, post) => {
     })
     .catch(error => console.log(error));
 };
+
+export const list = () => {
+  return fetch(`${process.env.REACT_APP_API_URL}/posts`, {
+    method: 'GET'
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
