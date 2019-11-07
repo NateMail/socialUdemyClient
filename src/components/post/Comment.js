8669ce51c31264d421cd9a94e60f6a11fcb7fe5c
nathanmailhiot <nathanjm12@outlook.com>
2019-11-07 12:59:36 -0500

    Show list of comments for post

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { comment, uncomment } from './apiPost';
 import { isAuthenticated } from '../../auth';
 import { Link } from 'react-router-dom';
+import DefaultProfile from '../../images/avatar.jpg';
 
 class Comment extends Component {
   state = {
@@ -31,6 +32,7 @@ class Comment extends Component {
   };
 
   render() {
+    const { comments } = this.props;
     return (
       <div>
         <h2 className="mt-5 mb-5">Leave a Comment</h2>
@@ -43,6 +45,37 @@ class Comment extends Component {
             />
           </div>
         </form>
+        {JSON.stringify(comments)}
+        <hr />
+        <div className="col-md-8 col-md-offset-2">
+          <h3 className="text-primary">{comments.length} Comments</h3>
+          <hr />
+          {comments.map((comment, i) => {
+            return (
+              <div key={i}>
+                <div>
+                  <Link to={`/user/${comment.postedBy._id}`}>
+                    <img
+                      style={{
+                        borderRadius: '50%',
+                        border: '1px solid black'
+                      }}
+                      className="float-left mr-2"
+                      width="30px"
+                      height="30px"
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${comment.postedBy._id}`}
+                      onError={i => (i.target.src = `${DefaultProfile}`)}
+                      alt={comment.postedBy.name}
+                    />
+                  </Link>
+                  <div>
+                    <p className="lead">{comment.text}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- src/components/post/SinglePost.js
+++ src/components/post/SinglePost.js
@@ -30,7 +30,8 @@ class SinglePost extends Component {
         this.setState({
           post: data,
           likes: data.likes.length,
-          like: this.checkLike(data.likes)
+          like: this.checkLike(data.likes),
+          comments: data.comments
         });
       }
     });
